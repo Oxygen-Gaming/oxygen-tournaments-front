1dc3bccce57d8c591e036944ab83d2691483d5a9
Hugo-Arroyo <hugo.arroyo@oxygengaming.es>
2025-03-04 15:12:51 +0100

    Resolving tailwind errors

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,19 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
+
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
-=======
+
   content: [],
->>>>>>> 764356e18df0df3fc1fbaaebf9bcb38377eee4c0
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
-};
-=======
-}
 
->>>>>>> 764356e18df0df3fc1fbaaebf9bcb38377eee4c0
+};
+
