b10a4a7a2d20b3067b45bb249b0a8ecec23af3b8
Hugo-Arroyo <hugo.arroyo@oxygengaming.es>
2025-03-05 13:14:30 +0100

    Create login form

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Register from "./components/RegisterComponent";  
-
+import Login from "./components/LoginComponent"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />  
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />   
       </Routes>
     </BrowserRouter>
 
