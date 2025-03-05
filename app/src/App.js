3774d96a7aca2046ee1fa130b2cdfef6bb7ee6bd
Hugo-Arroyo <hugo.arroyo@oxygengaming.es>
2025-03-05 12:17:39 +0100

    resolving problems

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Register from "./components/Register";  
+import Register from "./components/RegisterComponent";  
 
 function App() {
   console.log('Hola');
