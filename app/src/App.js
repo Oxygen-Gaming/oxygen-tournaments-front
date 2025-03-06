e92ca0eb79c7874cc38b6f504ef4e482980b61a1
Hidalgo611 <ma.hidalgo@profesionalsorolla.es>
2025-03-06 15:15:22 +0100

    Crear página home

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Register from "./components/RegisterComponent";  
 import Login from "./components/LoginComponent"
+import Welcome from "./components/WelcomeComponent"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Welcome />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />   
       </Routes>
