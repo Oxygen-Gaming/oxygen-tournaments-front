ea3b393d248e06c94ab7e8615a5ae458f2229faa
Marcos190390 <marcos.hidalgo@oxygengaming.es>
2025-03-11 15:32:55 +0100

    Update WelcomeComponent.js

--- app/src/components/WelcomeComponent.js
+++ app/src/components/WelcomeComponent.js
@@ -13,70 +13,6 @@ import Tarjeta6 from "../img/poster6.jpg"
 import Tarjeta7 from "../img/poster7.jpg"
 import Tarjeta8 from "../img/poster8.jpg"
 import Tarjeta9 from "../img/poster9.jpg"
-import Logo from "../img/Logo_OxygenGaming2.png";
-import Header from "./Header";
-import Footer from "./Footer";
-
-const rewards = [
-  {
-    title: "Watch Parties",
-    text: "Disfruta de eventos en vivo con otros miembros de OxyClub.",
-    image: "path/to/image1.jpg",
-  },
-  {
-    title: "Eventos de Comunidad",
-    text: "Participa en eventos exclusivos y conoce a otros gamers.",
-    image: "path/to/image2.jpg",
-  },
-  {
-    title: "Drops Exclusivos",
-    text: "Obtén recompensas únicas y exclusivas para miembros de OxyClub.",
-    image: "path/to/image3.jpg",
-  },
-  {
-    title: "Wallpapers",
-    text: "Descarga fondos de pantalla exclusivos para tu dispositivo.",
-    image: "path/to/image4.jpg",
-  },
-  {
-    title: "Descuento en Merchandising",
-    text: "Aprovecha descuentos especiales en nuestra tienda.",
-    image: "path/to/image5.jpg",
-  },
-  {
-    title: "Contenido exclusivo",
-    text: "Accede a contenido exclusivo solo para miembros de OxyClub.",
-    image: "path/to/image6.jpg",
-  },
-  {
-    title: "Acceso discord VIP",
-    text: "Únete a nuestro servidor VIP en Discord.",
-    image: "path/to/image7.jpg",
-  },
-  {
-    title: "Recompensas por misiones",
-    text: "Completa misiones y gana recompensas exclusivas.",
-    image: "path/to/image8.jpg",
-  },
-  {
-    title: "Guías competitivas",
-    text: "Accede a guías y estrategias para mejorar tu juego.",
-    image: "path/to/image9.jpg",
-  }
-]
-
-const Images = [
-
-  { image: Logo },
-  { image: Logo },
-  { image: Logo },
-  { image: Logo },
-  { image: Logo },
-  { image: Logo },
-  { image: Logo },
-  { image: Logo },
-  { image: Logo }
-]
 
 const OxygenGaming = () => {
   const cards = [
@@ -121,7 +57,6 @@ const OxygenGaming = () => {
         </div>
       </header>
       {/* MAIN CONTENT */}
-      <Header />
       <main className="flex items-center h-[calc(100vh-80px)] px-16 relative overflow-hidden">
         <svg
           className="absolute right-[-10%] top-1/2 -translate-y-1/2 w-[60%] opacity-10 z-[1]"
@@ -163,6 +98,7 @@ const OxygenGaming = () => {
           </div>
         </div>
       </main>
+      {/* SECCIÓN DE VENTAJAS */}
       <section className="py-16 px-8 bg-blue-800">
         <h2 className="text-center text-2xl md:text-3xl mb-8">Ventajas de unirte a OxyClub</h2>
         <div className="grid grid-cols-2 md:grid-cols-5 gap-x-2 gap-y-4 justify-items-center">
@@ -281,33 +217,10 @@ const OxygenGaming = () => {
               <a href="https://discord.gg/Udsstfpeze" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full no-underline transition-all">
                 <img src={discord} alt="Discord" className="w-full h-full rounded-full" />
               </a>
-        <h2 className="text-center text-2xl md:text-3xl mb-8">
-          Ventajas de unirte a OxyClub
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {Images.map((item, index) => (
-            <div key={index} className="p-8 rounded-lg shadow-md bg-blue-700 transition-transform duration-300 hover:scale-105 hover:shadow-md">
-              <img src={item.image} alt={`Ventaja ${index + 1}`} className="w-full h-full" />
             </div>
-          ))}
-          {
-          rewards.map((item, index) => (
-            <div
-              key={index}
-              className="p-8 rounded-lg shadow-md bg-blue-700 transition-transform duration-300 hover:scale-105 hover:shadow-md"
-              style={{
-                backgroundImage: `url(${item.image})`,
-                backgroundSize: "cover",
-              }}
-            >
-              <h3 className="text-xl mb-2">{item.title}</h3>
-              <p className="text-gray-300">{item.text}</p>
-
-            </div>
-          ))}
           </div>
-      </section>
-<Footer></Footer>
+        </div>
+      </footer>
     </div>
   );
 };
