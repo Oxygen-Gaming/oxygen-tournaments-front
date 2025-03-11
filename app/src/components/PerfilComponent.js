03986d61d2ba7a3413ccbdde17638dc695d1f5ff
Hugo-Arroyo <hugo.arroyo@oxygengaming.es>
2025-03-11 09:03:26 +0100

    Solucionando errores

--- app/src/components/PerfilComponent.js
+++ app/src/components/PerfilComponent.js
@@ -353,21 +353,21 @@ const Perfil = () => {
                     </div>
                     <div id="redes_sociales" className="flex flex-col items-center text-center flex-1 min-w-[250px]">
                         <a href="https://oxygengaming.es/">
-                        <img id="logo_footer" src={oxygen} alt="Oxygen Gaming Logo" className="w-[80%] max-w-[200px] mb-4" />
+                            <img id="logo_footer" src={oxygen} alt="Oxygen Gaming Logo" className="w-[80%] max-w-[200px] mb-4" />
                         </a>
                         <div className="flex gap-4 mt-2">
-                        <a href="https://twitter.com/GamingOxygen" target="_blank" rel="noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full border-[cyan] border-[1px] p-[10px] no-underline transition-all">
-                            <img src={twitter} alt="Twitter" className="w-full h-full rounded-full" />
-                        </a>
-                        <a href="https://instagram.com/GamingOxygen" target="_blank" rel="noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full border-[cyan] border-[1px] p-[10px] no-underline transition-all">
-                            <img src={instagram} alt="Instagram" className="w-full h-full rounded-full" />
-                        </a>
-                        <a href="https://www.tiktok.com/@oxg_esports?lang=es" target="_blank" rel="noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full border-[cyan] border-[1px] p-[10px] no-underline transition-all">
-                            <img src={tiktok} alt="TikTok" className="w-full h-full rounded-full" />
-                        </a>
-                        <a href="https://discord.gg/Udsstfpeze" target="_blank" rel="noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full border-[cyan] border-[1px] p-[10px] no-underline transition-all">
-                            <img src={discord} alt="Discord" className="w-full h-full rounded-full" />
-                        </a>
+                            <a href="https://twitter.com/GamingOxygen" target="_blank" rel="noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full border-[cyan] border-[1px] p-[10px] no-underline transition-all">
+                                <img src={twitter} alt="Twitter" className="w-full h-full rounded-full" />
+                            </a>
+                            <a href="https://instagram.com/GamingOxygen" target="_blank" rel="noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full border-[cyan] border-[1px] p-[10px] no-underline transition-all">
+                                <img src={instagram} alt="Instagram" className="w-full h-full rounded-full" />
+                            </a>
+                            <a href="https://www.tiktok.com/@oxg_esports?lang=es" target="_blank" rel="noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full border-[cyan] border-[1px] p-[10px] no-underline transition-all">
+                                <img src={tiktok} alt="TikTok" className="w-full h-full rounded-full" />
+                            </a>
+                            <a href="https://discord.gg/Udsstfpeze" target="_blank" rel="noreferrer" className="flex justify-center items-center w-10 h-10 rounded-full border-[cyan] border-[1px] p-[10px] no-underline transition-all">
+                                <img src={discord} alt="Discord" className="w-full h-full rounded-full" />
+                            </a>
                         </div>
                     </div>
                 </div>
