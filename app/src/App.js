7573ce07e5d4359f4fa893dcbc7648e7b2ef2456
Hidalgo611 <ma.hidalgo@profesionalsorolla.es>
2025-03-12 13:17:16 +0100

    App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./components/RegisterComponent";
+import Register from "./components/RegisterComponent";  
+import Login from "./components/LoginComponent"
+import Welcome from "./components/WelcomeComponent"
+import Competition from "./pages/CompetitionComponent";
 import Perfil from "./components/PerfilComponent";
-import Login from "./components/LoginComponent";
 import Rewards from "./components/RewardsComponent";
 import Missions from "./components/MissionsComponent";
-import Welcome from "./components/WelcomeComponent";
-import Profile from "./components/ContentComponent";
+import Content from "./components/ContentComponent";
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/register" element={<Register />} />
         <Route path="/perfil" element={<Perfil />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />   
+        <Route path="/competition" element={<Competition />} />
         <Route path="/rewards" element={<Rewards />} />
         <Route path="/missions" element={<Missions />} />
-        <Route path="/content" element={<Profile />} />
+        <Route path="/content" element={<Content />} />
       </Routes>
     </BrowserRouter>
   );
