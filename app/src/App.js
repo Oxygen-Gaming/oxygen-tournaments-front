c28c88066191720cf571426adce3602e3c896133
Enterprice07 <pi.javega@profesionalsorolla.es>
2025-03-13 15:18:49 +0100

    Error

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from '@components/RegisterComponent';
-import Perfil from '@components/PerfilComponent';
 import Login from '@components/LoginComponent';
 import Rewards from '@components/RewardsComponent';
 import Missions from '@components/MissionsComponent';
