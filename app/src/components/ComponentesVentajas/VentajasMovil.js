2a4ccebc633f1926c6e72b4a127085838985a5db
Hugo-Arroyo <hugo.arroyo@oxygengaming.es>
2025-04-07 12:36:37 +0200

    Añadimos las imagenes de las ventajas premium y corregimos los fallos en los giros de las tarjetas

--- app/src/components/ComponentesVentajas/VentajasMovil.js
+++ app/src/components/ComponentesVentajas/VentajasMovil.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const VentajaMovil = ({ link, text, shadow, background }) => {
+const VentajaMovil = ({ link, text, shadow, background, color }) => {
 
 
 
@@ -8,17 +8,19 @@ const VentajaMovil = ({ link, text, shadow, background }) => {
 
 
   return (
-    <div>
-      <div className="w-full h-full">
-          <div className={`shadow-[${shadow}] rounded-[10px] w-[100%] h-[100%]`}>
-            <img
-              src={link}
-              alt="Ventaja"
-              style={{ width: "100%", height: "100%", borderRadius: "10px" }}
-            />
+    <div className={`group h-[15rem] w-[15rem] sm:w-[30rem] sm:h-[30rem] [perspective:1000px]  rounded-xl`}>
+      <div className={`shadow-[${shadow}] h-full w-full rounded-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]`}>
+        <div className="h-full flex">
+          <img src={link} className="h-full w-full flex flex-col items-center justify-center rounded-xl" />
+        </div>
+        
+        <div className={`absolute inset-0 w-full h-full rounded-xl [transform:rotateY(180deg)] [backface-visibility:hidden]`} style={{ backgroundColor: background }}>
+          <div className="flex justify-center items-center flex-col p-[3rem]">
+            <h2 className={`text-[40px] text-[${color}] text-center font-bold`}>{text}</h2>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
--- app/src/components/componentesInicio/VentajasNormalesComponents.js
+++ app/src/components/componentesInicio/VentajasNormalesComponents.js
@@ -11,9 +11,6 @@ import posternormal4 from "@imgs/poster4.webp"
 import posternormal5 from "@imgs/poster5.webp"
 import posternormal6 from "@imgs/poster6.webp"
 
-import posterpremium1 from "@imgs/Recompensas_Premium1.jpg"
-import posterpremium2 from "@imgs/Recompensas_Premium2.jpg"
-import posterpremium3 from "@imgs/Recompensas_Premium3.jpg"
 const VentajasNormales = () => {
     return (
         <section className=" p-[15px] flex flex-col justify-center items-center mt-[5vh] gap-[5vh] overflow-hidden">
@@ -23,43 +20,43 @@ const VentajasNormales = () => {
             </div>
             <div className="hidden lg:flex flex-col items-center justify-center gap-[2rem]">
                 <div className="flex gap-[40px]">
-                    <VentajaPequeño link={posternormal1} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
-                    <VentajaPequeño link={posternormal2} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
-                    <VentajaGrande link={poster1} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
+                    <VentajaPequeño link={posternormal1} text={"Ventajas Free"} background={"#1AA9FF"} color="white"/>
+                    <VentajaPequeño link={posternormal2} text={"Ventajas Free"} background={"#1AA9FF"} color="white"/>
+                    <VentajaGrande link={poster1} text={"Ventajas Free"} background={" #1AA9FF"} color="white"/>
                 </div>
 
                 <div className="flex gap-[40px]">
-                    <VentajaGrande link={poster1} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
-                    <VentajaPequeño link={posternormal3} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
-                    <VentajaPequeño link={posternormal4} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
+                    <VentajaGrande link={poster1} text={"Ventajas Free"} background={"#1AA9FF"} color="white"/>
+                    <VentajaPequeño link={posternormal3} text={"Ventajas Free"} background={"#1AA9FF"} color="white"/>
+                    <VentajaPequeño link={posternormal4} text={"Ventajas Free"} background={"#1AA9FF"} color="white"/>
                     
                 </div>
 
                 <div className="flex gap-[40px]">
-                    <VentajaPequeño link={posternormal5} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
-                    <VentajaPequeño link={posternormal6} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
-                    <VentajaGrande link={poster1} text={"Ventajas Normales Free"} background={" #1AA9FF"} color="white"/>
+                    <VentajaPequeño link={posternormal5} text={"Ventajas Free"} background={"#1AA9FF"} color="white"/>
+                    <VentajaPequeño link={posternormal6} text={"Ventajas Free"} background={"#1AA9FF"} color="white"/>
+                    <VentajaGrande link={poster1} text={"Ventajas Free"} background={"#1AA9FF"} color="white"/>
                 </div>
             </div>
 
             <div className="lg:hidden flex flex-col items-center justify-center gap-[2rem]">
                 <div className="flex flex-col gap-[40px]">
-                    <VentajaMovil link={poster2} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
-                    <VentajaMovil link={poster2} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
-                    <VentajaMovil link={poster1} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
+                    <VentajaMovil link={posternormal1} text={"Ventajas Free"} background={"#1AA9FF"}/>
+                    <VentajaMovil link={posternormal2} text={"Ventajas Free"} background={"#1AA9FF"}/>
+                    <VentajaMovil link={poster1} text={"Ventajas Free"} background={"#1AA9FF"}/>
                 </div>
 
                 <div className="flex flex-col gap-[40px]">
-                    <VentajaMovil link={poster1} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
-                    <VentajaMovil link={poster2} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
-                    <VentajaMovil link={poster2} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
+                    <VentajaMovil link={poster1} text={"Ventajas Free"} background={"#1AA9FF"}/>
+                    <VentajaMovil link={posternormal3} text={"Ventajas Free"} background={"#1AA9FF"}/>
+                    <VentajaMovil link={posternormal4} text={"Ventajas Free"} background={"#1AA9FF"}/>
                     
                 </div>
 
                 <div className="flex flex-col gap-[40px]">
-                    <VentajaMovil link={poster2} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
-                    <VentajaMovil link={poster2} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
-                    <VentajaMovil link={poster1} text={"Ventajas Normales Free"} background={" #1AA9FF"}/>
+                    <VentajaMovil link={posternormal5} text={"Ventajas Free"} background={"#1AA9FF"}/>
+                    <VentajaMovil link={posternormal6} text={"Ventajas Free"} background={"#1AA9FF"}/>
+                    <VentajaMovil link={poster1} text={"Ventajas Free"} background={"#1AA9FF"}/>
                 </div>
             </div>
 
