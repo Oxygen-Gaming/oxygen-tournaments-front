320e82f219c039e0109a3f65eeaac8a228d48f2a
Hidalgo611 <ma.hidalgo@profesionalsorolla.es>
2025-04-09 10:10:40 +0200

    Banner y Fondo Ventajas

--- app/src/pages/VentajasPage.js
+++ app/src/pages/VentajasPage.js
@@ -12,9 +12,15 @@ const Ventajas = () => {
   
     return (
       
-      <div className="bg-blue-900 text-[white] font-roboto-condensed">
+      <div className="bg-[#1AA9FF] text-[white] font-roboto-condensed"> {/* Cambiado a bg-[#1AA9FF] */}
         <Header />
         <MenuHamburguesaNormal />
+        {/* BANNER */}
+        <div className="w-11/12 mx-auto h-80 bg-cover bg-center rounded-lg transition-all duration-1000 mt-8 relative">
+          <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg flex items-center justify-center">
+            <h1 className="text-5xl font-extrabold text-white">Ventajas</h1>
+          </div>
+        </div>
         <div className="flex flex-col justify-start items-center gap-[10vh]">
 
           <VentajasNormales />
