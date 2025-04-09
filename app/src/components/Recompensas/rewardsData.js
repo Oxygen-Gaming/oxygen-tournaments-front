81867d36c9f29d326126e17dc2bd2cab0273ff40
Hidalgo611 <ma.hidalgo@profesionalsorolla.es>
2025-04-09 11:35:21 +0200

    Secciones Detalladas Misiones y Recompensas

--- app/src/components/Recompensas/rewardsData.js
+++ app/src/components/Recompensas/rewardsData.js
@@ -8,18 +8,70 @@ import Google from '@imgs/Recompensas_10_Google.jpg';
 import Playstation from '@imgs/Recompensas_10_Playstation.jpg';
 import Silla from '@imgs/Recompensas_Silla.jpg';
 
-
 const rewards = [
-  { id: 1,  title: "Follow por parte de Oxygen", image: Follow , points: 100 },
-  { id: 2, title: "Clave aleatoria de Steam", image: Clave , points: 200 },
-  { id: 3, title: "Camiseta del club", image:Camiseta, points: 300 },
-  { id: 4, title:  "10€ Riot Points", image: Camiseta , points: 400 },
-  { id: 5, title:  "Ticket a sorteo", image: Ticket, points: 500 },
-  { id: 6, title: "1000 v-bucks Fortnite", image: Pavos, points: 600 },
-  { id: 7, title: "Mystery Box", image: Box,  points: 700 },
-  { id: 8, title: "10€ Google Play", image:  Google , points: 800 },
-  { id: 9, title: "10€ Playstation", image:  Playstation,  points: 900 },
-  { id: 10, title: "Silla Playseat Puma", image: Silla , points: 1000 },
+  { 
+    id: 1, 
+    title: "Follow por parte de Oxygen", 
+    image: Follow, 
+    points: 100, 
+    description: "¿Quieres unirte a la élite? Un follow exclusivo de Oxygen te conectará con una comunidad única. Por 100 puntos, estarás dentro del círculo del círculo de los mejores." 
+  },
+  { 
+    id: 2, 
+    title: "Clave aleatoria Steam", 
+    image: Clave, 
+    points: 200, 
+    description: "¿Buscas un nuevo juego? Una clave aleatoria de Steam te sorprenderá con un título increíble para tu biblioteca. Por 200 puntos, la aventura te espera." 
+  },
+  { 
+    id: 3, 
+    title: "Camiseta del Club", 
+    image: Camiseta, 
+    points: 300, 
+    description: "¿Listo para mostrar tu pasión? La camiseta del club te hará lucir con orgullo tu espíritu Oxygen. Por 300 puntos, vístete con estilo." 
+  },
+  { 
+    id: 4, 
+    title: "Ticket a Sorteo", 
+    image: Ticket, 
+    points: 500, 
+    description: "¿Sueñas con premios épicos? Un ticket a sorteo te da la chance de ganar recompensas increíbles. Por 500 puntos, la suerte está de tu lado." 
+  },
+  { 
+    id: 5, 
+    title: "1000 V-Bucks Fortnite", 
+    image: Pavos, 
+    points: 600, 
+    description: "¿Cómo vas a lucir tu victoria magistral? Con 1000 V-Bucks, podrás desbloquear skins exclusivas, bailes originales y otros ítems increíbles para personalizar tu experiencia. Por 600 puntos, estarás más cerca de tu próxima victoria épica." 
+  },
+  { 
+    id: 6, 
+    title: "Mystery Box", 
+    image: Box, 
+    points: 700, 
+    description: "¿Te gustan las sorpresas? Una Mystery Box te espera con un regalo único que te encantará. Por 700 puntos, la emoción es tuya." 
+  },
+  { 
+    id: 7, 
+    title: "Tarjeta 10€ Google Play", 
+    image: Google, 
+    points: 800, 
+    description: "¿Buscas diversión digital? Con 10 € en Google Play, tendrás acceso a apps, juegos y más para disfrutar al máximo. Por 800 puntos, el entretenimiento es tuyo." 
+  },
+  { 
+    id: 8, 
+    title: "Tarjeta 10€ PlayStation", 
+    image: Playstation, 
+    points: 900, 
+    description: "¿Quieres más juegos en PlayStation? Con 10 € en crédito, podrás descargar títulos y contenido para tu consola. Por 900 puntos, la diversión no para." 
+  },
+  { 
+    id: 9, 
+    title: "Silla PlaySeat PUMA", 
+    image: Silla, 
+    points: 10000, 
+    description: "Consigue la silla Playseat Puma y lleva tu experiencia de gaming al siguiente nivel." 
+  },
 ];
 
 export default rewards;
