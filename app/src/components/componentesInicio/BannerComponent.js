2a2e4b2009b0a27dcfd9d2f9e260aa265c2833e3
Enterprice07 <pi.javega@profesionalsorolla.es>
2025-04-09 08:56:58 +0200

    bracket

--- app/src/components/componentesInicio/BannerComponent.js
+++ app/src/components/componentesInicio/BannerComponent.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import Portada from "@imgs/Portada_de_Home.jpg";
 const Banner = () => {
     return (
-        <main className="flex items-center h-[calc(100vh-80px)] px-16 relative overflow-hidden md:mt-0 mt-0 pt-7 bg-blue-900" style={{backgroundImage: 'url("https://via.placeholder.com/1920x1080")', backgroundSize: 'cover', backgroundPosition: 'center',}}>
+        <main className="flex items-centr w-full h-[calc(100vh-80px)] px-16 relative overflow-hidden md:mt-0 mt-0 pt-7 bg-[#1AA9FF] mb-32" style={{backgroundImage: `url(${Portada})`, backgroundSize: 'cover', backgroundPosition: 'center',}}>
+            
+            <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div> {/* Restore the black overlay */}
+        <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-b from-transparent to-[#1AA9FF] z-10"></div>             
             <svg className="absolute right-[-10%] top-1/2 -translate-y-1/2 w-[60%] opacity-10 z-[1]" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
                 <path d="M250,50 L400,250 L250,450 L100,250 Z" className="fill-orange-400 opacity-20"/>
             </svg>
-            <div className="max-w-[600px] z-10 relative">
+            <div className="max-w-[600px] z-10 relative mt-16">
                 <h1 className="text-3xl mb-4 leading-[1.2]">VENTAJAS EXCLUSIVAS PARA{' '}<span className="text-blue-600">GAMERS</span>.</h1>
                 <h1 className="text-3xl mb-4 leading-[1.2]">VIVE LA MEJOR EXPERIENCIA GAMER CON{' '}<span className="text-blue-600">OXYCLUB</span>.</h1>
                 <p className="mb-8 text-gray-300">
