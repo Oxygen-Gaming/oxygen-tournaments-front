f5727fe82cb40ec151ef78d2173d8713f3923c95
pio-J <pio.andres@oxygengaming.es>
2025-04-14 17:28:26 +0200

    Update PerfilPage.js

--- app/src/pages/PerfilPage.js
+++ app/src/pages/PerfilPage.js
@@ -38,9 +38,8 @@ const Perfil = () => {
     };
 
     return (
-        <div className="bg-[#1AA9FF] text-white min-h-screen font-['Roboto_Condensed',sans-serif] flex flex-col pt-5 lg:pt-0">
+        <div className="bg-[#1AA9FF] text-white min-h-screen font-['Roboto_Condensed',sans-serif]">
             <Header />
-
             <MenuHamburguesaPerfil 
                 isOpen={isMenuOpen} 
                 setIsOpen={setIsMenuOpen} 
@@ -53,11 +52,6 @@ const Perfil = () => {
                 <p className="text-[30px]">Tu Perfil</p>
             </div>
             <div className="pl-[100px] px-[30px] hidden lg:flex lg:flex-col">
-
-            <MenuHamburguesaPerfil />
-           
-            <div className="pl-[100px] px-[30px] hidden lg:flex lg:flex-col lg:justify-center items-center mb-[10vh] mt-[10vh]">
-
                 <div className="flex text-[10px] gap-[7px] lg:text-[20px] lg:gap-[30px] w-[100%] font-bold">
                     <Enlace opcion={'perfil'} nombre={'Perfil'} setOpcionSeleccionada={setOpcionSeleccionada} />
                     <Enlace opcion={'inventario'} nombre={'Inventario'} setOpcionSeleccionada={setOpcionSeleccionada} />
@@ -69,7 +63,7 @@ const Perfil = () => {
                 </div>
                 <hr className="underline w-full"></hr>
             </div>
-            <div className="hidden lg:flex mb-[10vh]">
+            <div className="hidden lg:flex">
                 {renderContent()}
             </div>
             <Footer />   
