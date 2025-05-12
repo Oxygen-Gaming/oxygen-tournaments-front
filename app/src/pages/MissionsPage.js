bb30ce8374a4b90e9265d1e032ba32c4db594f10
davidkal30 <david.kalmuk@oxygengaming.es>
2025-05-12 15:26:00 +0200

    .

--- app/src/pages/MissionsPage.js
+++ app/src/pages/MissionsPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+zimport React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import missions from '../components/Misiones/missions';
